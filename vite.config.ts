8b1e8a00b257c566e167c0618e3f18bf59a6a445
Arkady Zelensky <arkasha9606@gmail.com>
2024-04-24 22:56:14 +0200

    prepare build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+// import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://ark9606.github.io/coin-keeper-react',
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //       // nested: resolve(__dirname, 'nested/index.html'),
+  //     },
+  //   },
+  // },
 })
