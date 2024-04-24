1452bef0eaa9564757a0b41d3d1a4a220d396608
Arkady Zelensky <arkasha9606@gmail.com>
2024-04-24 23:29:01 +0200

    fixed env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
+import 'dotenv/config'
 
 const APP_BASE_URL = process.env.APP_BASE_URL || '';
+console.log('APP_BASE_URL', APP_BASE_URL);
 
 // https://vitejs.dev/config/
 export default defineConfig({
