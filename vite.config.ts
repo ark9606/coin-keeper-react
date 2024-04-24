1c20fba6b496a7e68345cac393b4de7e7d5a3c4d
Arkady Zelensky <arkasha9606@gmail.com>
2024-04-24 23:44:55 +0200

    updated base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import sass from 'sass'
 import 'dotenv/config'
 
-const APP_BASE_URL = process.env.APP_BASE_URL || '';
-console.log('APP_BASE_URL', APP_BASE_URL);
+// const APP_BASE_URL = process.env.APP_BASE_URL || '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: APP_BASE_URL,
+  // base: APP_BASE_URL,
+  base: undefined,
   plugins: [react()],
   css: {
     preprocessorOptions: {
